c419794e414212863d01d1ea69876334853019a0
Trib20dev <Triburondev@gmail.com>
2026-03-23 20:13:26 +0100

    added its libraries

--- src/copy/DeadSnakeException.java
+++
@@ -1,9 +0,0 @@
-package copy;
-/**
- * Excepción capturable empleada para comunicar la muerte de la serpiente
- */
-public class DeadSnakeException extends Exception {
-
-	private static final long serialVersionUID = 2844101138038752887L;
-
-}
